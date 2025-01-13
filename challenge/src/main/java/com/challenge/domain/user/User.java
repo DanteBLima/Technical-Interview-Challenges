1fa7f503ac7ea114a07a5f61077b46d9416495a3
Dante BLima <dante.borges@sempreceub.com>
2025-01-13 18:19:58 -0300

    feat: Create User entity

---
+++ challenge/src/main/java/com/challenge/domain/user/User.java
@@ -0,0 +1,39 @@
+package com.challenge.domain.user;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private Long id;
+
+    private String firstName;
+
+    private String lastNAme;
+
+    @Column(unique = true)
+    private String document;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private BigDecimal balance;
+
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+
+
+}
---
+++ challenge/src/main/java/com/challenge/domain/user/UserType.java
@@ -0,0 +1,7 @@
+package com.challenge.domain.user;
+
+public enum UserType {
+
+    COMMON,
+    MERCHANT
+}
